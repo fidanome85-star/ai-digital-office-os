97943a9ab6affc38541f056b9ec8fbfffa97f37d
fidanome85-star <fidanome85@gmail.com>
2026-08-25 23:06:40 +0300

    Add __init__.py to core module
    Initialize core module with imports and exports.

---
+++ core/__init__.py
@@ -0,0 +1,16 @@
+from core.state import OfficeState, TaskMessage
+from core.constitution import ProjectConstitution
+from core.orchestrator import OfficeOrchestrator
+from core.artifact_manager import ArtifactManager
+from core.knowledge_brain import KnowledgeBrain
+from core.governance_auditor import GovernanceAuditor
+
+__all__ = [
+    "OfficeState",
+    "TaskMessage",
+    "ProjectConstitution",
+    "OfficeOrchestrator",
+    "ArtifactManager",
+    "KnowledgeBrain",
+    "GovernanceAuditor"
+]
